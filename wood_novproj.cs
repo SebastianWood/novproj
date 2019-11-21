de32d0feb1fa3d460e07ed8de24515dad144af8c
SebastianWood <54947163+SebastianWood@users.noreply.github.com>
2019-11-21 15:09:05 +0100

    Update wood_novproj.cs

--- wood_novproj.cs
+++ wood_novproj.cs
@@ -10,7 +10,45 @@ namespace NovemberProjekt_Sebastian_Wood
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("kalle djsgeojpsewåosdgijsejgsjoiagejogesskmdg'omphrmg");
+
+            Random generator = new Random();
+
+            int damage = generator.Next(0, 5);
+
+            //presentera fighter och skriv in klassen person här. 
+            Fighter fighter = new Fighter();
+
+            fighter.PrintInfo();
+
+            Console.WriteLine("Skriv in namn på fighter: ");
+
+            
+            {
+                fighter.namn = Console.ReadLine();
+
+            };
+
+            Console.WriteLine("" + fighter.namn + "!");
+
+            Console.WriteLine("Skriv in namn på fighter2: ");
+
+            Fighter fighter2 = new Fighter();
+            {
+                fighter2.namn = Console.ReadLine();
+
+            };
+
+
+            Console.WriteLine("" + fighter2.namn + "!");
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Nu börjar fighten! ");
+
+
+
+
 
         }
     }
